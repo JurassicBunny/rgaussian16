cbb09b02cc7f9843e904591eda152d249eb461e7
currymatthew <curry@curie.desktops.utk.edu>
2021-10-06 14:09:55 -0400

    testing

--- src/gaussian.rs
+++ src/gaussian.rs
@@ -1,1 +1,9 @@
 use serde::Deserialize;
+
+#[derive(Debug, Clone, Copy, Deserialize)]
+pub(crate) struct GaussConfig {}
+
+#[derive(Debug, Clone, Copy)]
+pub struct GaussInput {
+    config: GaussConfig,
+}
